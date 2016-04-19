d9c2734c08dbabf6a883ac52240233ba93f03f57
cindyk09 <cindy_kim88@hotmail.com>
2016-04-19 16:42:35 -0400

    refactored

--- article.rb
+++ article.rb
@@ -10,27 +10,76 @@
 #
 # Time limit: 60 minutes
 
+
 class Article
   attr_accessor :status_was, :status, :fact_checked
   # I had to alias it because attr_accessor does not allow methods to have "?"
   alias :fact_checked? :fact_checked
-  
+
   def send_email?
-    # Send the email only if the article's status changes in these exact ways:
-    if self.status_was == :assigned && (self.status == :approved || self.status == :rejected)
+    if approved_status? && rejected_status?
       true
-    elsif self.status_was == :review && (self.status == :approved || self.status == :rejected)
+    elsif was_assigned_or_reviewed? && approved_or_rejected?
       true
-    elsif self.status_was == :approved && self.status == :rejected
+    elsif followup? && self.fact_checked?
       true
-    elsif self.status_was == :rejected && self.status == :approved
-      true
-    elsif self.status_was != :followup && self.status == :followup && self.fact_checked?
-      true
-    elsif self.status_was != :review && self.status == :review
+    elsif reviewed?
       true
     else
       false
     end
   end
+
+  def approved_or_rejected?
+    self.status == :approved || self.status == :rejected
+  end
+
+  def was_assigned_or_reviewed?
+    self.status_was == :assigned || self.status_was == :review
+  end
+
+  def approved_status?
+    self.status_was == :approved || self.status == :approved
+  end
+
+  def followup?
+    self.status_was != :followup && self.status == :followup
+  end
+
+  def rejected_status?
+    self.status_was == :rejected || self.status == :rejected
+  end
+
+  def reviewed?
+    self.status_was != :review && self.status == :review
+  end
 end
+
+
+##############################################################################
+# Explanation:
+# Before I really dug into refactoring I decided to learn how to write tests for the
+# existing code. This way I can make sure that in the process of refactoring
+# I don't loose desired behavior along the way.
+# In order to make the test work I have added attr_accessor and learned that I need
+# to alias a method that ends in a "?" because attr_accessor does not recognize it.
+# Once I finished the test I looked at the code to see what it was doing.
+# It's trying to see if status_was is true for assigned, review, approved, or rejected.
+# Secondly it is seeing if status is true for approved, rejected, followup and review.
+# I started looking at the patterns for the first two lines that if status_was true it wasn't true for status itself.
+# I condensed it in one line to make more redable. Please see below for the code.
+# At this point everytime I am changing things I make sure I run my tests to make sure I am on the right track
+# Once everything is condensed I start creating helper methods to refactor it even more.
+
+
+    # if (self.status_was == :approved || self.status == :approved) && (self.status_was == :rejected || self.status == :rejected)
+    #   true
+    # elsif (self.status_was == :assigned || self.status_was == :review) && (self.status == :approved || self.status == :rejected)
+    #   true
+    # elsif self.status_was != :followup && self.status == :followup && self.fact_checked?
+    #   true
+    # elsif self.status_was != :review && self.status == :review
+    #   true
+    # else
+    #   false
+    # end
